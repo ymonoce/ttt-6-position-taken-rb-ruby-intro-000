c769dd9e2417ece1d377240cb6fbf001a2ca89f6
Chloe Atwood <atwoodksw@gmail.com>
2020-12-04 20:50:30 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,9 @@
 # code your #position_taken? method here!
 def position_taken?(board, index = "X")
 
-if board[index] != " "
-elsif board[index] != ""
-else board[index] != nil
-end
+board[index] != " "
+board[index] != ""
+
 
 
 
